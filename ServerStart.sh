c8d1b9c652ede1b07464614f0813609728f0ea2b
Boris Lachev <boris.lachevbg00@gmail.com>
2021-07-02 21:08:26 +0300

    Add script line

--- ServerStart.sh
+++ ServerStart.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 java -Xmx3200M -Xms1024M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:PermSize=128m -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar minecraft_server.jar nogui
 pause
